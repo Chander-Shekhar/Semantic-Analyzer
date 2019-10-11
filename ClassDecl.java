2c4e8dac1fb5548d12d1ed7cbfc95b533e01eab3
Chander-Shekhar <44772437+Chander-Shekhar@users.noreply.github.com>
2019-10-11 16:28:26 +0530

    Update ClassDecl.java

--- ClassDecl.java
+++ ClassDecl.java
@@ -8,13 +8,15 @@ public class ClassDecl
   public String pname = null;
   public HashMap<String, AST.attr> aList;
   public HashMap<String, AST.method> mList;
+  public Integer height;
   
-  public ClassDecl (String cname, String parent, HashMap<String, AST.attr> attrs, HashMap<String, AST.method> methods) {
+  public ClassDecl (String cname, String parent, HashMap<String, AST.attr> attrs, HashMap<String, AST.method> methods, Integer h) {
      name = cname;
      pname = parent;
      aList = new HashMap<String, AST.attr>();
      mList = new HashMap<String, AST.method>();
      aList.putAll(attrs);
      mList.putAll(methods);
+     height = h;
   }
 }
